860e5f13cd229f211c22fe1b75a7bfc02b10ac7a
Colin Middleton <46512256+middlec000@users.noreply.github.com>
2021-11-22 21:13:15 -0700

    Preprocessed CORD19

--- test/main.py
+++ test/main.py
@@ -1,37 +1,47 @@
 import sys
 sys.path.insert(1, 'C:/Users/colin/Documents/GitHub/fhdc/src/fhdc')
+import pickle
+from time import time
+import itertools
 
 from fhdc_model import FHDC_Model
 
 def main():
     
-    docs = {
-        "doc1": "one one one two", # one: 3, two: 1 -> one: 3
-        "doc0": "one one one three three", # one: 3, three: 2 -> one: 3, three: 2
-        "doc2": "two two three three" # two: 2, three: 2 -> two: 2, three: 2
-    }
+    #docs = {
+    #    "doc1": "one one one two", # one: 3, two: 1 -> one: 3
+    #    "doc0": "one one one three three", # one: 3, three: 2 -> one: 3, three: 2
+    #    "doc2": "two two three three" # two: 2, three: 2 -> two: 2, three: 2
+    #}
     # Vocabulary: {one: 6, two: 2, three: 3}
     # doc1 -> one: 3/6 = 0.5
     # doc0 -> one: 3/6 = 0.5, three: 2/4 = 0.5
     # doc2 -> two: 2/1 = 1, three: 2/4 = 0.5
 
+    filename = "C:\\Users\\colin\\Documents\\GitHub\\fhdc\\data\\dictionary_version\\docs_dict.pickle"
+
+    time0 = time()
+    docs = pickle.load(open(filename,'rb'))
+    time1 = time()
+
+    # num_docs = 5
+    # docs = dict(itertools.islice(docs.items(), num_docs)) 
+
+    print(f"Loading Corpus (s): {time1-time0}")
+
     model = FHDC_Model()
 
-    corpus, vocabulary = model.preprocess(docs, doc_min_frequency=2, corpus_min_frequency=2, return_processed=True)
+    time2 = time()
+    corpus, vocabulary = model.preprocess(docs, doc_min_frequency=2, corpus_min_frequency=5, return_processed=True)
+    time3 = time()
 
-    print()
-    print('Corpus')
-    print(corpus.__str__(vocabulary=vocabulary, verbosity=2))
-    print()
-    print('Vocabulary')
-    print(vocabulary)
-    print()
+    outfile_name = "C:\\Users\\colin\\Documents\\GitHub\\fhdc\\data\\preprocessed_version\\corpus.pickle"
 
-    #model.cluster()
+    outfile = open(outfile_name, 'wb')
+    pickle.dump(corpus, outfile)
+    outfile.close()
 
-    #verbosity = 2
-    #print(f'Clustering Summary (Verbosity = {verbosity})')
-    #print(model.summary(verbosity=verbosity))
+    print(f"Processing Corpus (s): {time3 - time2}")
     
     return
 
--- test/preprocess_cord19.py
+++ test/preprocess_cord19.py
@@ -1,17 +1,55 @@
 import json
 import pickle
 from langdetect import detect
+import os
+from time import time
 
-def detect_english(doc):
+def detect_english(text):
     # Default langauge is English (most docs are)
     # All we care about are the 'en' ones
     language = 'en'
+    threshold = 100
     try:
-        if len(doc) > 25:
-            language = detect(' '.join(doc[:25]))
-        elif len(doc) > 0:
-            language = detect(' '.join(doc[:len(doc)]))
+        if len(text) > threshold:
+            language = detect(text[:threshold])
+        elif len(text) > 0:
+            language = detect(text)
     except Exception as e:
         # If cannot detect language, drop doc
         return False
     return language == 'en'
+
+def doc_to_dict(folder: str, num_docs: int=None) -> dict:
+    files = os.listdir(folder)
+    if num_docs is not None:
+        files = files[:num_docs]
+    corpus = {}
+    for file in files:
+        with open(folder + "\\" + file) as opened_file:
+            doc = json.load(opened_file)
+            text = ' '.join([chunk['text'].lower() for chunk in doc['body_text']])
+            if detect_english(text):
+                corpus[file] = text
+    return corpus
+
+folder = "C:\\Users\\colin\\Documents\\GitHub\\fhdc\\data\\archive\\document_parses\\pdf_json"
+
+outfile_name = "C:\\Users\\colin\\Documents\\GitHub\\fhdc\\data\\dictionary_version\\docs_dict.pickle"
+
+# total number of docs: 278,523
+# Est. time = 2:15
+num_docs = None
+
+time0 = time()
+
+corpus = doc_to_dict(folder=folder, num_docs=num_docs)
+
+time1 = time()
+
+outfile = open(outfile_name, 'wb')
+pickle.dump(corpus, outfile)
+outfile.close()
+
+time2 = time()
+
+print(f"Number of documents: {num_docs}\nCreating Corpus (s): {time1-time0}\nSaving Corpus (s): {time2 - time1}")
